1eaadc77dd7a17e6ce91607a3f8c6c82d36e8226
henryhoangduong <henryhoangduong@gmail.com>
2025-01-18 11:03:42 +0700

    configure security

--- src/main/java/com/example/Blog/repository/RoleEntity.java
+++ src/main/java/com/example/Blog/repository/RoleEntity.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import javax.management.relation.Role;
 
 public interface RoleEntity extends JpaRepository<Role, Long> {
+
 }
--- src/main/java/com/example/Blog/repository/UserEntity.java
+++ src/main/java/com/example/Blog/repository/UserEntity.java
@@ -4,4 +4,5 @@ import com.example.Blog.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserEntity extends JpaRepository<Users, Long> {
+
 }
