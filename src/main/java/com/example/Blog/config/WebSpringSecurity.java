2b840bee00555ed34c2ea5aeb3459df7671c5d39
henryhoangduong <henryhoangduong@gmail.com>
2025-01-21 12:33:58 +0700

    security config

--- src/main/java/com/example/Blog/config/WebSpringSecurity.java
+++ src/main/java/com/example/Blog/config/WebSpringSecurity.java
@@ -4,12 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSpringSecurity {
+
+    private UserDetailsService userDetailsService;
+
+    public WebSpringSecurity(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,4 +39,5 @@ public class WebSpringSecurity {
                         .permitAll());
         return http.build();
     }
+
 }
