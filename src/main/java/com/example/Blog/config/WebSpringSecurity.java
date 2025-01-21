620e7c4384d0acb3d192b88747f59ba9f24eedea
henryhoangduong <henryhoangduong@gmail.com>
2025-01-21 12:34:25 +0700

    security config

--- src/main/java/com/example/Blog/config/WebSpringSecurity.java
+++ src/main/java/com/example/Blog/config/WebSpringSecurity.java
@@ -1,7 +1,9 @@
 package com.example.Blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,6 +40,11 @@ public class WebSpringSecurity {
                         .loginProcessingUrl("/login")
                         .permitAll());
         return http.build();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
 }
